d06ab0a45b54c869167a4b4cc759bca2239d5eb4
matthew-plusprogramming <matthewlinplusprogramming@gmail.com>
2021-04-06 18:02:49 -0700

    i

---
+++ src/components/ExampleComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Style imports
+// import '../styles/.scss';
+
+// Page imports
+
+// Component imports
+
+// Context imports
+
+const ExampleComponent: React.FC = () => {
+  return <div></div>;
+};
+
+export default ExampleComponent;
